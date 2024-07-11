cec3ac0de9c3c5ee82493554d016e71cb9c3df23
Brydizzy <107905417+Brydizzy@users.noreply.github.com>
2024-07-10 20:46:12 -0700

    Add files via upload

---
+++ Arrowfunc.js
@@ -0,0 +1,18 @@
+const double = (arr) => {
+    return arr.map((val) => {
+      return val * 2;
+    });
+  };
+
+  
+
+  const double = (arr) => arr.map((val) => val * 2);
+
+
+
+  const squareAndFindEvens = (numbers) => {
+    const squares = numbers.map((num) => num ** 2);
+    const evens = squares.filter((square) => square % 2 === 0);
+    return evens;
+  };
+  
